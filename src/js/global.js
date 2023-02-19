fa32ac321418332aa1602695bc2f16740f6f7e62
Don Osborne <intastella@users.noreply.github.com>
2023-02-19 14:49:38 -0800

    adding auto switching to theme switcher, shift click to reset

--- src/js/global.js
+++ src/js/global.js
@@ -1,37 +1,68 @@
-function toggleColorTheme() {
-  if (document.documentElement.classList.contains("theme-dark")) {
-    document.documentElement.classList.remove("theme-dark");
-    document.documentElement.classList.add("theme-light");
-    localStorage.setItem("colorTheme", "light");
-  } else {
-    document.documentElement.classList.remove("theme-light");
-    document.documentElement.classList.add("theme-dark");
-    localStorage.setItem("colorTheme", "dark");
-  }
-}
-
 function initColorThemeSwitch() {
   const buttonColorThemeSwitch = document.querySelector(".js-theme-switch");
   const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-  const savedTheme = localStorage.getItem("colorTheme");
+  
+  function toggleColorTheme(event) {
+    const savedTheme = localStorage.getItem("colorTheme");
 
-  if (buttonColorThemeSwitch !== null) {
-    buttonColorThemeSwitch.addEventListener("click", function () {
-      toggleColorTheme();
-    });
+    if (event.type == "click") {
+      // console.log('click');
+      if (event.shiftKey) {
+        // console.log('click - shift was down');
+        localStorage.removeItem("colorTheme");
+        toggleColorTheme("none");
+      } else {
+        if (savedTheme == "dark") {
+          // console.log('saved was dark, switch to light');
+          document.documentElement.classList.remove("theme-dark");
+          localStorage.setItem("colorTheme", "light");
+        } 
+        if (savedTheme == "light") {
+          // console.log('saved was light, switched to dark');
+          document.documentElement.classList.add("theme-dark");
+          localStorage.setItem("colorTheme", "dark");
+        }
+        if (savedTheme == null) {
+          if (prefersDarkScheme.matches) {
+            // console.log('auto was dark, switch to light');
+            document.documentElement.classList.remove("theme-dark");
+            localStorage.setItem("colorTheme", "light");
+          } 
+          if (!prefersDarkScheme.matches) {
+            // console.log('auto was light, switch to dark');
+            document.documentElement.classList.add("theme-dark");
+            localStorage.setItem("colorTheme", "dark");
+          }
+        }
+      }
+    } else {
+      // console.log('non click');
+      if (savedTheme == "dark") {
+        // console.log('auto applied saved dark');
+        document.documentElement.classList.add("theme-dark");
+      }
+      if (savedTheme == "light") {
+        // console.log('auto applied saved light');
+        document.documentElement.classList.remove("theme-dark");
+      }
+      if (savedTheme == null) {
+        if (prefersDarkScheme.matches) {
+          // console.log('auto applied system dark');
+          document.documentElement.classList.add("theme-dark");
+        } 
+        if (!prefersDarkScheme.matches) {
+          // console.log('auto applied system light');
+          document.documentElement.classList.remove("theme-dark");
+        }
+      }
+    }
   }
 
-  if (savedTheme == null) {
-    if (prefersDarkScheme.matches) {
-      document.documentElement.classList.add("theme-dark");
-    }
-  } else {
-    if (savedTheme == "dark") {
-      document.documentElement.classList.add("theme-dark");
-    }
-  }
+  buttonColorThemeSwitch.addEventListener("click", toggleColorTheme);
+  prefersDarkScheme.addEventListener("change", toggleColorTheme);
+  toggleColorTheme("none");
 }
 
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   initColorThemeSwitch();
 })
