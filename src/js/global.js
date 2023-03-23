bab3e7284066627aea4b0764aaa967552ab39063
Don Osborne <intastella@users.noreply.github.com>
2023-03-23 00:04:42 -0700

    auto wallpaper color switching added, theme switch bugs fixed

--- src/js/global.js
+++ src/js/global.js
@@ -1,11 +1,10 @@
 function initColorThemeSwitch() {
-  const ThemeMenu = document.querySelector(".js-theme-menu");
-  const ThemeMenuOverlay = document.querySelector(".js-header-theme-menu-overlay");
+  const themeMenu = document.querySelector(".js-theme-menu");
+  const themeMenuOverlay = document.querySelector(".js-header-theme-menu-overlay");
   const buttonThemeMenuButton = document.querySelector(".js-theme-menu-button");
   const themeButtons = document.querySelectorAll(".js-theme-button");
   const wallpaperButtons = document.querySelectorAll(".js-wallpaper-button");
   const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-  const savedTheme = localStorage.getItem("colorTheme");
   
   // https://thegermancoder.com/2018/10/04/how-to-remove-classes-by-prefix-in-vanilla-javascript/
   function removeClassByPrefix(node, prefix) {
@@ -15,55 +14,49 @@ function initColorThemeSwitch() {
   }
   
   function manualSetColorTheme(el) {
-    // console.log('manual set');
+    // console.log('manual set theme');
     var selectedButton = el.target.closest('.js-theme-button');
     var selectedTheme = selectedButton.dataset.theme;
     // console.log('theme='+selectedTheme);
     removeClassByPrefix(document.documentElement, 'theme-');
+    document.documentElement.classList.add("theme-"+selectedTheme);
 
-    if (selectedTheme == "dark") {
-      document.documentElement.classList.add("theme-dark");
-      localStorage.setItem("colorTheme", selectedTheme);
-      toggleThemeMenu();
-    } 
-    if (selectedTheme == "light") {
-      document.documentElement.classList.add("theme-light");
-      localStorage.setItem("colorTheme", selectedTheme);
-      toggleThemeMenu();
-    }
     if (selectedTheme == "auto") {
       localStorage.removeItem("colorTheme");
+      toggleThemeMenu();
+      autoSetColorTheme();
+    } else {
+      localStorage.setItem("colorTheme", selectedTheme);
       toggleThemeMenu();
     }
   }
 
   function autoSetColorTheme() {
-    // console.log('auto set');
-    document.documentElement.className = "";
-    if (savedTheme == "dark") {
-      console.log('auto applied saved dark');
-      document.documentElement.classList.add("theme-dark");
-    }
-    if (savedTheme == "light") {
-      // console.log('auto applied saved light');
-      document.documentElement.classList.add("theme-light");
-    }
+    const savedTheme = localStorage.getItem("colorTheme");
+    // console.log('auto set theme');
+    removeClassByPrefix(document.documentElement, 'theme-');
+    
     if (savedTheme == null) {
       if (prefersDarkScheme.matches) {
         // console.log('auto applied system dark');
+        document.documentElement.classList.add("theme-auto");
         document.documentElement.classList.add("theme-dark");
       } 
       if (!prefersDarkScheme.matches) {
+        document.documentElement.classList.add("theme-auto");
         // console.log('auto applied system auto');
       }
+    } else {
+      // console.log('auto applied saved '+savedTheme);
+      document.documentElement.classList.add("theme-"+savedTheme);
     }
   }
 
   function manualSetWallpaperColor(el) {
-    console.log('wallpaper color click');
+    // console.log('manual set wallpaper');
     var selectedButton = el.target;
     var selectedColor = selectedButton.dataset.color;
-    console.log('color='+selectedColor);
+    // console.log('color='+selectedColor);
 
     if (selectedColor !== "classic") {
       removeClassByPrefix(document.documentElement, 'wallpaper-');
@@ -73,20 +66,26 @@ function initColorThemeSwitch() {
     }
     else {
       removeClassByPrefix(document.documentElement, 'wallpaper-');
+      localStorage.removeItem("wallpaperColor");
       toggleThemeMenu();
     }
   }
 
+  function autoSetWallpaperColor() {
+    const savedWallpaperColor = localStorage.getItem("wallpaperColor");
+    document.documentElement.classList.add("wallpaper-"+savedWallpaperColor);
+  }
+
   function toggleThemeMenu() {
-    if (ThemeMenu.classList.contains('tmpl-header__theme-menu--active')) {
-      ThemeMenu.classList.remove('tmpl-header__theme-menu--active');
+    if (themeMenu.classList.contains('tmpl-header__theme-menu--active')) {
+      themeMenu.classList.remove('tmpl-header__theme-menu--active');
     } else {
-      ThemeMenu.classList.add('tmpl-header__theme-menu--active');
+      themeMenu.classList.add('tmpl-header__theme-menu--active');
     }
   }
 
-  if (ThemeMenu !== null) {
-    ThemeMenuOverlay.addEventListener("click", toggleThemeMenu);
+  if (themeMenu !== null) {
+    themeMenuOverlay.addEventListener("click", toggleThemeMenu);
     buttonThemeMenuButton.addEventListener("click", toggleThemeMenu);
 
     Array.prototype.forEach.call(themeButtons, function (el, i) {
@@ -104,6 +103,7 @@ function initColorThemeSwitch() {
   
   prefersDarkScheme.addEventListener("change", autoSetColorTheme);
   autoSetColorTheme();
+  autoSetWallpaperColor();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
